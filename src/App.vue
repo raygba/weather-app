f7c92779ad3d0a64f64f1b3ac95e771a2da2fbd0
raygba <rkraiss@my.waketech.edu>
2025-12-02 14:55:18 -0500

    feat: search, weather cards

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,38 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import SearchInput from './components/SearchInput.vue'
+import WeatherCard from './components/WeatherCard.vue'
+
+const places = ref([])
+const addPlace = (data) => {
+  places.value.push(data)
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <main>
+    <h1>The Weather</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+    <!-- Date -->
+    <div class="text-center mb-6">
+      {{ new Date().toLocaleDateString('en-us', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+      }) }}
     </div>
-  </header>
 
-  <RouterView />
+    <!-- Search -->
+    <div>
+      <SearchInput @place-data="addPlace" />
+    </div>
+
+    <!-- Weather card -->
+    <div class="grid grid-cols-2 gap-4">
+      <div v-for="(place, idx) in places" :key="idx">
+        <WeatherCard :place="place" />
+      </div>
+    </div>
+  </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
---
+++ src/components/WeatherForecastDay.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+    day: Object
+})
+</script>
+
+<template>
+    <div class="mb-2 flex items-center justify-between">
+        <table class="w-full">
+            <tr>
+                <!-- day of the week -->
+                <td class="w-1/3">{{ new Date(day.date).toLocaleDateString('en-us', { weekday: 'long' }) }}</td>
+
+                <!-- icon -->
+                <td class="w-1/3">
+                    <img src="" alt="icon" width="30" class="mx-auto" />
+                </td>
+
+                <!-- high/low temp -->
+                <td class="w-1/3 text-right">[Max temperature] / [Min temperature]</td>
+            </tr>
+        </table>
+    </div>
+</template>
